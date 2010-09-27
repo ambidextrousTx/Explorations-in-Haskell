696e0cfebee00d58693bd278c53bd624e9fa1605
ambidextrousTx <ravisinha@my.unt.edu>
2010-09-27 01:58:01 -0700

    Typo - fixed

--- funcs.hs
+++ funcs.hs
@@ -1,7 +1,7 @@
 -- Automatic geometric series generator 
 -- Very limited for now, takes a fixed format input
 -- Produces an infinite list
--- Example: take 5 (gs [3, 15..]) => [3, 15, 75]
+-- Example: take 3 (gs [3, 15..]) => [3, 15, 75]
 -- Improvements aimed for: 
 -- *accept upper limit
 -- *handle data types properly, don't use truncate
